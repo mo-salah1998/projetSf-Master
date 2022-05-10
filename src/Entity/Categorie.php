29de186d5f4a3261be9e88de08e949dee98fe1ad
wael zantour <waelzantour1@gmail.com>
2022-05-10 12:50:18 +0200

    chore: Test Commit

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -7,7 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\ORM\Mapping as ORM;
-
+//test
 /**
  * @ORM\Entity(repositoryClass=CategorieRepository::class)
  */
